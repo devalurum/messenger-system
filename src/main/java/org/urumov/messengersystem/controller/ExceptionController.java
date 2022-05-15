c7e59fcb1d6ede02ab3ea452d716990429ca9591
Alexander Urumov <sahaheh@mail.ru>
2022-05-15 13:50:34 +0800

    updated ExceptionController

--- src/main/java/org/urumov/messengersystem/controller/ExceptionController.java
+++ src/main/java/org/urumov/messengersystem/controller/ExceptionController.java
@@ -4,17 +4,21 @@ import io.swagger.v3.oas.annotations.Hidden;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.urumov.messengersystem.model.error.ErrorDescription;
-import org.urumov.messengersystem.model.error.ErrorResponse;
-import org.urumov.messengersystem.model.error.ValidationErrorResponse;
+import org.urumov.messengersystem.domain.dto.error.ErrorDescription;
+import org.urumov.messengersystem.domain.dto.error.ErrorResponse;
+import org.urumov.messengersystem.domain.dto.error.ValidationErrorResponse;
 
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ValidationException;
 import java.util.List;
 
 import static java.util.stream.Collectors.joining;
@@ -41,6 +45,18 @@ public class ExceptionController {
     @ResponseStatus(HttpStatus.CONFLICT)
     @ExceptionHandler(IllegalStateException.class)
     public ErrorResponse conflict(IllegalStateException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(ValidationException.class)
+    public ErrorResponse badRequest(ValidationException exception) {
+        return new ErrorResponse(exception.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    @ExceptionHandler(AccessDeniedException.class)
+    public ErrorResponse handleAccessDeniedException(AccessDeniedException exception) {
         return new ErrorResponse(exception.getMessage());
     }
 
