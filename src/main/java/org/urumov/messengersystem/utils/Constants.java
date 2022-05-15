14e42f5d633328320d3901bb4286429e8c6f7058
Alexander Urumov <sahaheh@mail.ru>
2022-05-15 13:53:41 +0800

    created constants

---
+++ src/main/java/org/urumov/messengersystem/utils/Constants.java
@@ -0,0 +1,10 @@
+package org.urumov.messengersystem.utils;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class Constants {
+    public static final String JWT = "JWT";
+    public static final String BEARER = "Bearer ";
+    public static final String BEARER_AUTH_SCHEME = "bearerAuth";
+}
