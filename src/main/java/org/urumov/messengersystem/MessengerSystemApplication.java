34d196b56791b88d73f977327dda91822c3cfe1c
Alexander Urumov <sahaheh@mail.ru>
2022-05-15 14:00:46 +0800

    added annotation in entry point

--- src/main/java/org/urumov/messengersystem/MessengerSystemApplication.java
+++ src/main/java/org/urumov/messengersystem/MessengerSystemApplication.java
@@ -2,8 +2,14 @@ package org.urumov.messengersystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@ConfigurationPropertiesScan
+@EnableWebMvc
 public class MessengerSystemApplication {
 
     public static void main(String[] args) {
