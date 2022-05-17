bd38fa415de3254fc45ad6163111ac85ec543537
Alexander Urumov <sahaheh@mail.ru>
2022-05-17 11:03:12 +0800

    created custom object mapper bean with javaTimeModule and JtsModule

---
+++ src/main/java/org/urumov/messengersystem/configuration/ObjectMapperConfig.java
@@ -0,0 +1,27 @@
+package org.urumov.messengersystem.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.n52.jackson.datatype.jts.JtsModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ObjectMapperConfig {
+
+    @Bean
+    public JtsModule jtsModule() {
+        return new JtsModule();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        var mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        mapper.registerModule(new JtsModule());
+        return mapper;
+    }
+}
